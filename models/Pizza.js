9a94ce505f12101a71a54aafe67f2496d203db7a
Christi-Lee <christileemc@gmail.com>
2021-07-06 16:04:36 -0700

    created Comment model and associated in Pizza.js

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,28 @@
-const {  Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
-},
-createdBy:{
-    type: String
-},
-createdAt: {
-    type: Date, 
-    default: Date.now
-},
-size: {
-    type: String, 
-    default: 'Large'
-},
-toppings: []
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: {
+    type: String,
+  },
+  size: {
+    type: String,
+    default: "Large",
+  },
+  toppings: [],
 });
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza Model
 module.exports = Pizza;
