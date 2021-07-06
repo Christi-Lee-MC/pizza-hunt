097e8a952301b1eae9fefdb67255e3748b420693
Christi-Lee <christileemc@gmail.com>
2021-07-06 16:40:07 -0700

    populating fields

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,36 @@
 const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
-  },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  comments: {
-    type: String,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
-});
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
+        type: String
+      },
+      createdBy: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      size: {
+        type: String,
+        default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
