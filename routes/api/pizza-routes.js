c12ac6ea56f576f57dd5dfe1426369880f3dedef
Christi-Lee <christileemc@gmail.com>
2021-07-06 15:18:36 -0700

    finished api/routrs

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getAllPizza, 
+    getPizzaById, 
+    createPizza, 
+    updatePizza, 
+    deletePizza,
+    getPizzaById
+} = require('../../controllers/pizza-controller');
+const { update } = require('../../models/Pizza');
+
+// /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
