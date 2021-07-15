0a97451463c25a013ad51a6e739bc156ce94522c
Christi-Lee <christileemc@gmail.com>
2021-07-15 09:02:43 -0700

    added event listener and uploadPizza

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -28,7 +28,7 @@ request.onsuccess = function(event) {
     if (navigator.onLine) {
       // we haven't created this yet, 
     //   but we will soon, so let's comment it out for now
-      // uploadPizza();
+      uploadPizza();
     }
   };
 
@@ -95,4 +95,8 @@ getAll.onsuccess = function() {
     // log error here
     console.log(event.target.errorCode);
   };
+
+  // listen for app coming back online
+window.addEventListener('online', uploadPizza);
+
 };
