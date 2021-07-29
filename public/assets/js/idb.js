770cd437cbd891ed43115438f01ab6830dcad33b
Christi-Lee <christileemc@gmail.com>
2021-07-29 13:22:51 -0700

    added onsuccess event handler

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -15,3 +15,25 @@ request.onupgradeneeded = function (event) {
     // set it to have an auto incrementing primary key of sorts
     db.createObjectStore('new_pizza', {autoIncrement: true});
 };
+
+// upon a successful request
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store
+    // (from onupgradeneeded event above)
+    // or simply established a connection
+    // save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, 
+    // if yes run uploadPizza() function 
+    //  to send all local db data to api
+    if (navigator.onLine) {
+        // we haven't created this yet, but we will soon, so let's comment it out for now
+    // uploadPizza();
+  }
+};
+
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
